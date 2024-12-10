271d1bc98abf1d7ac7412566309daa8edf5b10d7
cyndevsecops <my-email>
2024-12-11 01:36:23 +0700

    fix 3

--- flask-fix.py
+++ flask-fix.py
@@ -1,1 +1,10 @@
-# test
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "my simple code"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8081)
